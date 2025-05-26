243b7b524c15a23ffd92651e837db8f4e45db7c2
kimgunwoo <guncow0603@gmail.com>
2025-05-26 20:56:02 +0900

    first commit

---
+++ src/test/java/com/gracefulpickup/GracefulPickupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gracefulpickup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GracefulPickupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

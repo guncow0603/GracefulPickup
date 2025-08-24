1b09acbf2bcc37ce9ec7d207ccb46c5240f432cc
kimgunwoo <guncow0603@gmail.com>
2025-08-24 18:17:16 +0900

    우아한 정리 웹페이지-배포용 페이지 업로드

--- js/header.js
+++ js/header.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    fetch("/header.html")
+    fetch("./header.html")
         .then((res) => res.text())
         .then((data) => {
             const header = document.getElementById("header");
